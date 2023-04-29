2d181eb548517c8a19240a68d229270f62d54671
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-04-29 22:48:17 +0200

    feat(authors): Add authors find by slugf

--- src/api/author/controllers/author.js
+++ src/api/author/controllers/author.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::author.author');
+module.exports = createCoreController('api::author.author', ({ strapi }) => ({
+    async findBySlug(ctx) {
+        const { slug } = ctx.params;
+
+        ctx.query = {
+            filters: { slug },
+            ...ctx.query,
+        };
+
+        const { data } = await super.find(ctx);
+        const author = data[0] ? data[0] : 'Not found';
+        return { author };
+    },
+}));
