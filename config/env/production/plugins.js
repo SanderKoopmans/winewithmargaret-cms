98788e04159a47a70de340fa1eaf1507c4b7cf18
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-05-08 16:28:36 +0200

    feat(populate): Add plugin for deep population, change capital letters

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 5, // Default is 5
+    }
+  },
   seo: {
     enabled: true,
   },
---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  'strapi-plugin-populate-deep': {
+    config: {
+      defaultDepth: 5, // Default is 5
+    }
+  },
+  seo: {
+    enabled: true,
+  },
+});
