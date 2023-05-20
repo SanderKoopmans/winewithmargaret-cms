4e11c6ab7b34cdc3d4e82d5402e625347d9d5bc3
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-05-20 22:10:56 +0200

    feat(author): Add additional author info, slider, image and text components

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -11,11 +11,34 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
         const { slug } = ctx.params;
 
         ctx.query = {
-            filters: { slug },
-            ...ctx.query,
+          filters: { slug },
+          ...ctx.query,
         };
-
+        
         const { data } = await super.find(ctx);
+        const query = strapi.db.query('api::post.post');
+        await Promise.all(
+          data.map(async (item, index) => {
+            const foundItem = await query.findOne({
+              where: {
+                id: item.id,
+              },
+              populate: ['createdBy', 'updatedBy'],
+            });
+            
+            data[index].attributes.createdBy = {
+              id: foundItem.createdBy.id,
+              firstname: foundItem.createdBy.firstname,
+              lastname: foundItem.createdBy.lastname,
+            };
+            data[index].attributes.updatedBy = {
+              id: foundItem.updatedBy.id,
+              firstname: foundItem.updatedBy.firstname,
+              lastname: foundItem.updatedBy.lastname,
+            };
+          })
+        );
+
         const post = data[0] ? data[0] : 'Not found';
         return { post };
     },
